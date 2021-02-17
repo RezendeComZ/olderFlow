e28ce923ca5f921855c338011f44c6ba80192444
Gabriel Rezende <g@brielrezen.de>
2021-02-17 19:04:07 -0300

    basic functions working

--- src/olderFlow.js
+++ src/olderFlow.js
@@ -1,10 +1,25 @@
-console.log('v2')
+console.log('v9')
 
 function qSelector(select) {
   return document.querySelector(select)
 }
 
-let postDateRaw  = qSelector('time')
+let dateNow = new Date();
 
-let postDate = postDateRaw.dateTime.split(/[:+T+\-]/)
-postDateRaw.style.backgroundColor = 'blue'
+let postDateDiv  = qSelector('time')
+let postDateArray = postDateDiv.dateTime.split(/[:+T+\-]/)
+let postDate = new Date(postDateArray[0], postDateArray[1],postDateArray[2], postDateArray[3],postDateArray[4]);
+let differenceInTime = dateNow.getTime() - postDate.getTime()
+let differenceInDays = parseInt(differenceInTime / (1000 * 3600 * 24)); 
+
+if (differenceInDays > 4200) {
+  postDateDiv.style.backgroundColor = 'green'
+} else {
+  postDateDiv.style.backgroundColor = 'red'
+}
+
+console.log(postDate)
+console.log(differenceInTime)
+console.log(differenceInDays)
+
+
