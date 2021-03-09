4e209679cc66c4ee7c3028b60afb0f698cf85bdd
Gabriel Rezende <g@brielrezen.de>
2021-03-09 10:45:44 -0300

    initial google support

---
+++ src/google.js
@@ -0,0 +1,12 @@
+// Google - english
+
+let datesTxtDivs = document.querySelectorAll('.f')
+datesTxtDivs[0].style.color = 'green'
+
+// dates[0].parentElement.parentElement.parentElement.parentElement.childNodes[0].childNodes[0]
+
+
+// let datesTxtDivs = Array.from(document.getElementsByClassName('f'))
+// datesTxtDivs[0].style.color = 'green'
+// timeStyleAndProcessing(answerDate, div, true, false)
+// document.getElementsByClassName('f')[0].parentElement.parentElement.parentElement.parentElement.getElementsByTagName('a')[0].href
