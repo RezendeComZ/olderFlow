be779a42aec450f0264b42a109c4d6aab3d4e3c9
Gabriel Rezende <g@brielrezen.de>
2021-03-11 20:12:39 -0300

    needs better styling, but working

--- src/scripts/duckduckgo.js
+++ src/scripts/duckduckgo.js
@@ -1,5 +1,8 @@
 let duckResults = Array.from(document.querySelectorAll('.result__a')).filter(link => link.href.includes('stack'))
 
-duckResults.forEach(link => link.href += "?direct")
+duckResults.forEach(link => {
+  // link.href += "?direct"
+  link.outerHTML += `<br><a href="${link.href}?direct" style="background-color: rgb(50, 205, 50); border-radius: 15px; padding: 5px 8px 5px 8px; margin-right: 5px">✅ Best answer</a>`
+})
 
-duckResults[0].outerHTML += `<br><a href="nada"?direct" style="background-color: rgb(50, 205, 50); border-radius: 15px; padding: 5px 8px 5px 8px; margin-right: 5px">✅ Best answer</a>`
+// duckResults[0].outerHTML += `<br><a href="nada"?direct" style="background-color: rgb(50, 205, 50); border-radius: 15px; padding: 5px 8px 5px 8px; margin-right: 5px">✅ Best answer</a>`
